83bea8da8650b33b38082d50b38a9ca9a76a5831
Vlad Maraev <lichreg@ya.ru>
2020-10-11 18:37:45 +0200

    check start once again

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -21,125 +21,7 @@ object MainScenario : Scenario() {
             }
             action {
                 logger.info("/Start")
-                reactions.say("Привет, Марк. ")
-				reactions.go("/")
             }
-        }
-
-        state("Obsession") {
-            activators {
-                intent("Obsession")
-            }
-            action {
-                reactions.sayRandom("Марк, давай поговорим об этом позже.", "Об этом я тебе еще расскажу.")
-                reactions.go("/Initiate")
-            }
-        }
-
-        state("Initiate") {
-            action {
-                reactions.sayRandom(
-                    "А сейчас можно я тебя сама кое о чем спрошу?",
-                    "Можно я тебя кое-что спрошу?"
-                )
-            }
-            state("Yes") {
-                activators {
-                    intent("Answer:Yes")
-                }
-                action {
-                    reactions.go("/Ask")
-                }
-            }
-            state("No") {
-                activators {
-                    intent("Answer:No")
-                }
-                action {
-                    reactions.say("Ну ладно.")
-                }
-            }
-        }
-
-        state("Ask") {
-            action {
-                val question = selectQuestion()
-                reactions.go(question)
-            }
-
-            state("Cakes") {
-                action {
-					reactions.say("Вот скажи мне, что тебе больше нравится: Медовик или Наполеон?")
-                }
-                state("Napoleon") {
-                    activators {
-                        intent("Answer:Napoleon")
-                    }
-                    action {
-                        reactions.say("Да, это вкусно!")
-                        reactions.go("/End")
-                    }
-                }
-                state("Medovik") {
-                    activators {
-                        intent("Answer:Medovik")
-                    }
-                    action {
-                        reactions.say("Да, он ничего. Но Наполеон мне все-таки как-то больше по душе.")
-                        reactions.go("/End")
-                    }
-                }
-                fallback {
-                    reactions.say("Я такого, кажется, не пробовала.")
-                    reactions.go("/End")
-                }
-            }
-
-			// "Игра в магазин"
-            state("Supermarket") {
-                var counter = 0
-                activators { intent("Request:Supermarket") }
-                action {
-                    reactions.say("Вот ты приходишь в магазин и например я продавец. Ты должен что-то купить, например, мама или папа тебя попросили: у нас закончились хлеб и молоко, сходи купи пожалуйста в магазин. Что ты будешь делать?")
-                }
-                fallback {
-                    counter += 1
-                    val lim = (1..3).shuffled().take(1)[0]
-                    if (counter > lim) {
-                        counter = 0
-                        reactions.go("../Cashier")
-                    } else {
-                        reactions.sayRandom("угу", "что дальше?", "а дальше что?")
-                    }
-                }
-
-                state("Cashier") {
-                    action { reactions.say("Хорошо. а на кассе ты что говоришь?") }
-                    fallback { reactions.go("../1") }
-                    state("1") {
-                        action { reactions.say("Марк, ну допустим вот я продавец, да? и если у тебя не хватает денег и ты взял молоко дороже, чем у тебя есть денег, и я говорю \"у вас не хватает 15 рублей\". Что тогда?") }
-                        fallback { reactions.go("../2") }
-                        state("2") {
-                            action { reactions.say("А если ты уже на кассе. Там за тобой стоят люди, они ждут, пока ты оплатишь покупку. Пожалуйста, оплатите покупку, не задерживайте остальных людей. Что ты будешь делать?") }
-                            fallback { reactions.go("../end") }
-                            state("end") {
-                                action {
-                                    reactions.say("Правильный ответ - это отойти от кассы, взять другое молоко и купить его.")
-                                    reactions.go("/Reward")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-		state("/") {
-            action {
-                logger.info("/")
-                reactions.say("Спроси у меня что-нибудь.")
-            }
-
 
             state("WhatDoYouDo") {
                 activators { intent("Ask:WhatDoYouDo") }
@@ -212,7 +94,122 @@ object MainScenario : Scenario() {
                     reactions.go("/End")
                 }
             }
+
+
         }
+
+        state("Obsession") {
+            activators {
+                intent("Obsession")
+            }
+            action {
+                reactions.sayRandom("Марк, давай поговорим об этом позже.", "Об этом я тебе еще расскажу.")
+                reactions.go("/Initiate")
+            }
+        }
+
+
+        state("Initiate") {
+            action {
+                reactions.sayRandom(
+                    "А сейчас можно я тебя сама кое о чем спрошу?",
+                    "Можно я тебя кое-что спрошу?"
+                )
+            }
+            state("Yes") {
+                activators {
+                    intent("Answer:Yes")
+                }
+                action {
+                    reactions.go("/Ask")
+                }
+            }
+            state("No") {
+                activators {
+                    intent("Answer:No")
+                }
+                action {
+                    reactions.say("Ну ладно.")
+                }
+            }
+        }
+
+        state("Ask") {
+            action {
+                val question = selectQuestion()
+                reactions.go(question)
+            }
+
+            state("Cakes") {
+                action {
+					reactions.say("Вот скажи мне, что тебе больше нравится: Медовик или Наполеон?")
+                }
+                state("Napoleon") {
+                    activators {
+                        intent("Answer:Napoleon")
+                    }
+                    action {
+                        reactions.say("Да, это вкусно!")
+                        reactions.go("/End")
+                    }
+                }
+                state("Medovik") {
+                    activators {
+                        intent("Answer:Medovik")
+                    }
+                    action {
+                        reactions.say("Да, он ничего. Но Наполеон мне все-таки как-то больше по душе.")
+                        reactions.go("/End")
+                    }
+                }
+                fallback {
+                    reactions.say("Я такого, кажется, не пробовала.")
+                    reactions.go("/End")
+                }
+            }
+
+			// "Игра в магазин"
+            state("Supermarket") {
+                var counter = 0
+                globalActivators { intent("Request:Supermarket") }
+                action {
+                    reactions.say("Вот ты приходишь в магазин и например я продавец. Ты должен что-то купить, например, мама или папа тебя попросили: у нас закончились хлеб и молоко, сходи купи пожалуйста в магазин. Что ты будешь делать?")
+                }
+                fallback {
+                    counter += 1
+                    val lim = (1..3).shuffled().take(1)[0]
+                    if (counter > lim) {
+                        counter = 0
+                        reactions.go("../Cashier")
+                    } else {
+                        reactions.sayRandom("угу", "что дальше?", "а дальше что?")
+                    }
+                }
+
+                state("Cashier") {
+                    action { reactions.say("Хорошо. а на кассе ты что говоришь?") }
+                    fallback { reactions.go("../1") }
+                    state("1") {
+                        action { reactions.say("Марк, ну допустим вот я продавец, да? и если у тебя не хватает денег и ты взял молоко дороже, чем у тебя есть денег, и я говорю \"у вас не хватает 15 рублей\". Что тогда?") }
+                        fallback { reactions.go("../2") }
+                        state("2") {
+                            action { reactions.say("А если ты уже на кассе. Там за тобой стоят люди, они ждут, пока ты оплатишь покупку. Пожалуйста, оплатите покупку, не задерживайте остальных людей. Что ты будешь делать?") }
+                            fallback { reactions.go("../end") }
+                            state("end") {
+                                action {
+                                    reactions.say("Правильный ответ - это отойти от кассы, взять другое молоко и купить его.")
+                                    reactions.go("/Reward")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
 	
         state("Reward") {
             action {
@@ -238,7 +235,7 @@ object MainScenario : Scenario() {
                 "Этого я пока что не знаю.",
                 "Извини, я не знаю."
             )
-            reactions.go("/Initiate")
+            reactions.go("/Start")
         }
     }
 }
