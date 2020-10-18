017dd41caca30fc055ded6028cfbd794763f95b5
Vlad Maraev <lichreg@ya.ru>
2020-10-18 15:02:30 +0200

    no global intent for greet

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -43,7 +43,7 @@ object MainScenario : Scenario() {
             globalActivators {
                 event(AimyboxEvent.START)
                 regex("/start")
-				intent("Greet")
+		// intent("Greet")
             }
             action {
 
