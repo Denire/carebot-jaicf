2e99ce4ea1a254922e384bc268d92f1593582e5e
Vlad Maraev <lichreg@ya.ru>
2020-10-22 10:55:58 +0200

    temp context

--- src/main/kotlin/com/justai/jaicf/template/controllers/NoinputController.kt
+++ src/main/kotlin/com/justai/jaicf/template/controllers/NoinputController.kt
@@ -3,7 +3,7 @@ package com.justai.jaicf.template.controllers
 import com.justai.jaicf.context.BotContext
 
 class NoinputController(context: BotContext) {
-    var attempts: Int? by context.session
+    var attempts: Int? by context.temp
     var attemptsTemp: Int? by context.temp
 
 }
